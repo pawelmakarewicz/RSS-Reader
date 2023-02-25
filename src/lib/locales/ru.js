7eb686f20c5bbd433ae5b736eb06b405731b7049
Pawel Makarewicz <pawelmakarewicz.1996@gmail.com>
2023-02-25 20:39:43 +0100

    add i18next

---
+++ src/lib/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    signUpForm: {
+      validationResult: {
+        alreadyExist: 'RSS уже существует',
+        wrongURL: 'Ссылка должна быть валидным URL',
+        urlIsOk: 'RSS успешно загружен',
+      },
+    },
+  },
+};
--- src/lib/validator/watcher.js
+++ src/lib/validator/watcher.js
@@ -1,58 +1,69 @@
 import onChange from 'on-change';
 import * as Yup from 'yup';
+import i18n from 'i18next';
+import resources from '../locales/index.js';
 import render from './formRender.js';
 
-const urlValidationSchema = Yup.string().url('Ссылка должна быть валидным URL');
-
 export default function validateForm() {
-  const initialStateForm = {
-    rssLinks: [],
-    formUiState: {
-      state: null,
-      description: null,
+  const i18nextInstance = i18n.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: true,
+    resources: {
+      ru: resources.ru,
     },
-    errors: {},
-  };
+  })
+    .then(() => {
+      const initialStateForm = {
+        rssLinks: [],
+        formUiState: {
+          state: null,
+          description: null,
+        },
+        errors: {},
+      };
+      const urlValidationSchema = Yup.string().url(i18nextInstance.t('signUpForm.validationResult.wrongURL'));
+      const elements = {
+        rssForm: document.querySelector('form'),
+        rssInput: document.querySelector('#url-input'),
+        rssFormFeedback: document.querySelector('.feedback'),
+      };
 
-  const elements = {
-    rssForm: document.querySelector('form'),
-    rssInput: document.querySelector('#url-input'),
-    rssFormFeedback: document.querySelector('.feedback'),
-  };
+      const watchedState = onChange(initialStateForm, render(elements));
 
-  const watchedState = onChange(initialStateForm, render(elements));
+      function changeUiState(newState, descriptionOfNewState) {
+        watchedState.formUiState.state = newState;
+        watchedState.formUiState.description = descriptionOfNewState;
+      }
 
-  function changeUiState(newState, descriptionOfNewState) {
-    watchedState.formUiState.state = newState;
-    watchedState.formUiState.description = descriptionOfNewState;
-  }
+      function addErr(e) {
+        watchedState.errors = e;
+      }
 
-  function addErr(e) {
-    watchedState.errors = e;
-  }
+      function addLink(link) {
+        watchedState.rssLinks.push(link);
+      }
 
-  function addLink(link) {
-    watchedState.rssLinks.push(link);
-  }
+      function checkUrl(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const newRssLink = formData.get('url');
+        if (watchedState.rssLinks.includes(newRssLink)) {
+          changeUiState('invalid', i18nextInstance.t('signUpForm.validationResult.alreadyExist'));
+          return;
+        }
+        urlValidationSchema.validate(newRssLink)
+          .then(() => {
+            changeUiState('valid', i18nextInstance.t('signUpForm.validationResult.urlIsOk'));
+            addLink(newRssLink);
+            addErr(null);
+          })
+          .catch((err) => {
+            changeUiState('invalid', err.message);
+            addErr(err);
+          });
+      }
 
-  function checkUrl(e) {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const newRssLink = formData.get('url');
-    if (watchedState.rssLinks.includes(newRssLink)) {
-      changeUiState('invalid', 'RSS уже существует');
-      return;
-    }
-    urlValidationSchema.validate(newRssLink)
-      .then(() => {
-        changeUiState('valid', 'RSS успешно загружен');
-        addLink(newRssLink);
-        addErr(null);
-      })
-      .catch((err) => {
-        changeUiState('invalid', err.message);
-        addErr(err);
-      });
-  }
-  elements.rssForm.addEventListener('submit', checkUrl);
+      elements.rssForm.addEventListener('submit', checkUrl);
+    });
 }
