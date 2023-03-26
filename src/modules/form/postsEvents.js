caca2b60b6129ef8cccb44cfd3d0a79b5cad26ee
Pawel Makarewicz <pawelmakarewicz.1996@gmail.com>
2023-03-26 15:39:34 +0200

    fix Ui sates of Post

--- src/modules/form/postsEvents.js
+++ src/modules/form/postsEvents.js
@@ -4,6 +4,13 @@ export default function postsWatcher(elements, state) {
     const id = element.getAttribute('data-id');
     if (id) {
       state.changeUiStateOfPost(Number(id));
+      const link = [...element.parentNode.childNodes].filter((node) => node.tagName === 'A')[0];
+      console.log(link);
+      link.className = '';
+      link.classList.add('fw-normal', 'link-secondary');
+      if (element.tagName === 'BUTTON') {
+        console.log('aaaa');
+      }
     }
   }
   elements.rssPostsContainer.addEventListener('click', onClickChangeUiState);
